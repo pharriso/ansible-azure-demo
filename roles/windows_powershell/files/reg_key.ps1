0a311380b17022b40e3eaab472a75a82b9df32b8
Pat Harrison <pharriso@redhat.com>
2019-08-07 15:44:03 +0100

    win powershell value

--- roles/windows_powershell/files/reg_key.ps1
+++ roles/windows_powershell/files/reg_key.ps1
@@ -1,1 +1,1 @@
-Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name 'UserAuthentication' -value '1'
+Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name 'UserAuthentication' -value '0'
